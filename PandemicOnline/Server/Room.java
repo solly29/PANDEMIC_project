1fed42ad54cf5794feabd413b8bc1e99e061c45c
solly29 <44259984+solly29@users.noreply.github.com>
2019-11-07 23:25:05 +0900

    메소드 이름 수정 및 주석 추가
    userListGame과 userListChat으로 나눔

--- PandemicOnline/Server/Room.java
+++ PandemicOnline/Server/Room.java
@@ -27,29 +27,30 @@ public class Room{
 		System.out.println(name);
 		user.userAdd(name);
 	}
-	
+	//방에 해당 유저를 지운다.
 	public void RoomUserListDel(String name) {
 		user.userDel(name);
 	}
-	
+	//방이름이 있으면 반환
 	public void setRoomName(String str) {
 		roomName = str;
 	}
-	
+	//이름 설정
 	public String getRoomName() {
 		return roomName;
 	}
 	
 	//이건 보류(방장 시스템)
 	public String GetKing() {
-		return user.getUserList().keySet().iterator().next();
+		return user.getUserListGame().keySet().iterator().next();
 	}
 	
-	//내 방에 있는 유저들의 정보를 가져온다.
-	public Hashtable<String, DataOutputStream> getUserList(){
-		return user.getUserList();
+	//내 방에 있는 유저들의 게임소켓의 스트림을 가져옴
+	public Hashtable<String, DataOutputStream> getUserListGame(){
+		return user.getUserListGame();
 	}
 	
+	//방에 있는 유저들의 채팅소켓의 스트림을 가져옴
 	public Hashtable<String, DataOutputStream> getUserListChat(){
 		return user.getUserListChat();
 	}
