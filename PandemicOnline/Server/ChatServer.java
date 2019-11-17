9f75c2a01600f23c4cece08ea59f0a65d696ada9
cksgh3377 <55428959+cksgh3377@users.noreply.github.com>
2019-11-17 17:13:17 +0900

    Update ChatServer.java
    방이름출력을위해 살짝 수정

--- PandemicOnline/Server/ChatServer.java
+++ PandemicOnline/Server/ChatServer.java
@@ -8,7 +8,7 @@ import java.util.Hashtable;
 
 public class ChatServer implements Runnable{
 	//채팅 서버이다
-	private int roomNum = 0;
+	private String roomName = "";
 	private Hashtable<String, DataOutputStream> list = null;//유저 리스트 보내기용
 	private DataInputStream input = null;
 	private Socket s;
@@ -25,7 +25,7 @@ public class ChatServer implements Runnable{
 	 * 전체 유저 리스트 객체에서 닉네임에 대한 소켓을 가지고 온다.
 	 */
 	
-	public ChatServer(int num, String name) {
+	public ChatServer(String roomname, String name) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		//나의 소켓과 입력스트림을 받는다.
@@ -39,14 +39,14 @@ public class ChatServer implements Runnable{
 		}
 		
 		//해당 방 번호에 대한 룸객체안에 유저들의 리스트를 가지고온다.
-		roomNum = num;
-		list = MainServer.roomList.get(roomNum).getUserListChat();
+		roomName = roomname;
+		list = MainServer.roomList.get(roomName).getUserListChat();
 	}
 	
 	//방바꾸기
-	public void ChangeRoomNumber(int num) {
-		roomNum = num;
-		list = MainServer.roomList.get(num).getUserListChat();
+	public void ChangeRoomNumber(String name) {
+		roomName = name;
+		list = MainServer.roomList.get(name).getUserListChat();
 	}
 	
 	//유저 리스트에 받은 입력 정보를 보낸다.
@@ -89,7 +89,7 @@ public class ChatServer implements Runnable{
 				}
 				else {
 					str = str.substring(4);
-					System.out.println(roomNum);
+					System.out.println(roomName);
 				}
 				if(input == null || str == null) break;   // 입력이 아무것도 들어오지 않으면 탈출
 				sendAll(name+">"+str);
@@ -97,11 +97,11 @@ public class ChatServer implements Runnable{
 		}catch (Exception e) {
 			// TODO: handle exception
 		}finally {
-			MainServer.roomList.get(roomNum).RoomUserListDel(name);//방에 유저 삭제
-			System.out.println(MainServer.roomList.get(roomNum).getUserListChat());
-			if(MainServer.roomList.get(roomNum).getUserListChat().isEmpty()) {
+			MainServer.roomList.get(roomName).RoomUserListDel(name);//방에 유저 삭제
+			System.out.println(MainServer.roomList.get(roomName).getUserListChat());
+			if(MainServer.roomList.get(roomName).getUserListChat().isEmpty()) {
 				System.out.println("샂게함");
-				MainServer.roomList.remove(roomNum);
+				MainServer.roomList.remove(roomName);
 			}
 			
 		}
