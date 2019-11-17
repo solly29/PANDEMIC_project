1f3e4fcb24fb59fa8089c5c36c60abe4d0e0d696
dudrms9373 <55429152+dudrms9373@users.noreply.github.com>
2019-11-17 14:41:51 +0900

    Update GameRoom.java

--- PandemicOnline/Server/GameRoom.java
+++ PandemicOnline/Server/GameRoom.java
@@ -9,16 +9,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class GameRoom extends MainThread{
 	private int roomNum = 0;
 	private Hashtable<String, DataOutputStream> list = null;//유저 리스트 보내기용
 	private DataInputStream input = null;
+	private DataOutputStream output = null;
 	private Socket gsocket;
 	private String name; 
-	private Runnable ChatRun;
-	private Thread ChatTh;
+	private ChatServer ChatRun;
 	
 	public GameRoom() {
 		// TODO Auto-generated constructor stub
@@ -30,17 +31,24 @@ public class GameRoom extends MainThread{
 	 * 전체 유저 리스트 객체에서 닉네임에 대한 소켓을 가지고 온다.
 	 */
 	
-	public GameRoom(int num, String name) {
+	public GameRoom(int num, String name, ChatServer Chat) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
+		this.ChatRun = Chat;
+		ChatRun.ChangeRoomNumber(num);
 		//나의 소켓과 입력스트림을 받는다.
 		gsocket = LobbyServer.userList.get(name)[0];
 		try {
 			input = new DataInputStream(gsocket.getInputStream());
+			output = new DataOutputStream(gsocket.getOutputStream());
+			output.writeUTF("true");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//이건 방장을 클라가 표시를 할수있으면 주석 풀기~!!
+		//sendAll("[제어]방장:"+MainServer.roomList.get(roomNum).GetKing()); 방장이 누구인지 모든 플레이어에게 보낸다.
 		
 		//해당 방 번호에 대한 룸객체안에 유저들의 리스트를 가지고온다.
 		roomNum = num;
@@ -48,10 +56,8 @@ public class GameRoom extends MainThread{
 		
 		/*게임 서버를 만들경우 채팅은 스레드로 처리한다.
 		 * 나중에는 주석처리 지우기*/
-		ChatRun = new ChatServer(roomNum, name);
-		ChatTh = new Thread(ChatRun);
-		ChatTh.start();
 		
+		System.out.println("방장은 : "+MainServer.roomList.get(roomNum).GetKing());
 		/*
 		 * 이 부분부터 서비스 시작이다.
 		 * 일단 채팅만 구현함! 
@@ -61,16 +67,61 @@ public class GameRoom extends MainThread{
 				
 				String str = input.readUTF();   // 클라이언트로부터 채팅 받아옴
 				System.out.println(str);
-				if(str.equals("start"))
-					System.out.println("게임시작");
+				if(str.equals("Ready")) {
+					System.out.println("준비");
+					sendAll("["+name+"]Ready");
+				}
+				else if(str.equals("[방장]Ready")) {
+					System.out.println("방장준비");
+					sendAll("Start");
+				}
+				else if(str.equals("unReady")) {
+					System.out.println("준비 취소");
+					sendAll("["+name+"]unReady");
+				}
+				else if(str.equals("Start")) {
+					System.out.println("시작");
+					MainServer.roomList.get(roomNum).setStart(true);
+					//게임이 시작이 된다.
+					if(name.equals(MainServer.roomList.get(roomNum).GetKing())) {
+						//현재 유저가 방장일경우
+						//이문장을 수행한다.
+					}
+					while(true) {
+						str = input.readUTF();   // 클라이언트로부터 채팅 받아옴
+						System.out.println(str);
+						if(input == null || str == null) break;   // 입력이 아무것도 들어오지 않으면 탈출
+						if(str.substring(0,4).equals("[제어]")) {
+							str = str.substring(4);
+							//게임에서 서버가 관여 함
+							//예를들어 승패 여부
+						}
+						sendAll(str);
+					}
+				}else {
+					break;
+				}
+				
+				/*
+				 * 카드 배부
+				 * 시작 플레이어 선정
+				 * */
 			}
 		}catch (Exception e) {
 			// TODO: handle exception
 		}finally {
-			MainServer.roomList.get(roomNum).RoomUserListDel(name);//방에 유저 삭제
+			if(name.equals(MainServer.roomList.get(roomNum).GetKing())) {//만약에 방장이 나일경우 바뀐 방장이 누구인지 모든 플레이어에게 보낸다.
+				MainServer.roomList.get(roomNum).RoomUserListDel(name);//방에 유저 삭제
+				//sendAll("[제어]방장:"+MainServer.roomList.get(roomNum).GetKing()); 방장이 누구인지 모든 플레이어에게 보낸다.
+			}else {
+				MainServer.roomList.get(roomNum).RoomUserListDel(name);//방에 유저 삭제
+			}
+			
+			System.out.println("방장은 : "+MainServer.roomList.get(roomNum).GetKing());
 			System.out.println(MainServer.roomList.get(roomNum).getUserListChat());
+			
 			if(MainServer.roomList.get(roomNum).getUserListChat().isEmpty()) {
-				System.out.println("샂게함");
+				System.out.println("삭제함");
 				MainServer.roomList.remove(roomNum);
 			}
 		}
