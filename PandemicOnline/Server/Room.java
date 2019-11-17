f894962eb9db2ce9f1b7574f2abd54a2d13a9162
dudrms9373 <55429152+dudrms9373@users.noreply.github.com>
2019-11-17 14:40:37 +0900

    Update Room.java

--- PandemicOnline/Server/Room.java
+++ PandemicOnline/Server/Room.java
@@ -1,6 +1,7 @@
 package Server;
 
 import java.io.DataOutputStream;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 /*
@@ -9,49 +10,92 @@ import java.util.Hashtable;
  * 여기서는 방의 모든 정보, 제어권?을 갖는다.
  */
 
-public class Room{
+public class Room {
 	private UserList user = null;
 	private String roomName = "";
-	
+	private String kingName = "";
+	private boolean start = false;
+
 	public Room() {
 		// TODO Auto-generated constructor stub
 	}
-	//해당 닉네임 또는 아이디의 유저를 유저 클레스로 추가한다.(방을 생성을 할경우)
+
+	// 해당 닉네임 또는 아이디의 유저를 유저 클레스로 추가한다.(방을 생성을 할경우)
 	public Room(String name) {
 		// TODO Auto-generated constructor stub
 		System.out.println(name);
-		user = new UserList(name);//유저 객체를 생성
+		kingName = name;
+		user = new UserList(name);// 유저 객체를 생성
 	}
-	//방에 입장할경우 이 메소드를 이용해서 유저 클래스로 추가한다.
+
+	// 방에 입장할경우 이 메소드를 이용해서 유저 클래스로 추가한다.
 	public void RoomUserListAdd(String name) {
 		System.out.println(name);
 		user.userAdd(name);
 	}
-	//방에 해당 유저를 지운다.
+
+	// 방에 해당 유저를 지운다.
 	public void RoomUserListDel(String name) {
-		user.userDel(name);
+		try {
+			user.userDel(name);
+			kingName = user.getUserListChat().keys().nextElement();
+		}catch (Exception e) {
+			// TODO: handle exception
+			kingName = "";
+			System.out.println("유저 없음");
+		}
 	}
-	//방이름이 있으면 반환
+	
+	public void setStart(boolean start) {
+		this.start = start;
+	}
+	
+	public boolean getStart() {
+		return start;
+	}
+
+	// 방이름이 있으면 반환
 	public void setRoomName(String str) {
 		roomName = str;
 	}
-	//이름 설정
+
+	// 이름 설정
 	public String getRoomName() {
 		return roomName;
 	}
-	
-	//이건 보류(방장 시스템)
+
+	// 방장 시스템
 	public String GetKing() {
-		return user.getUserListGame().keySet().iterator().next();
+		return kingName;
 	}
 	
-	//내 방에 있는 유저들의 게임소켓의 스트림을 가져옴
-	public Hashtable<String, DataOutputStream> getUserListGame(){
+	//방 인원수 반환
+	public int getRoomSize() {
+		return user.getUserListChat().size();
+	}
+
+	// 내 방에 있는 유저들의 게임소켓의 스트림을 가져옴
+	public Hashtable<String, DataOutputStream> getUserListGame() {
 		return user.getUserListGame();
 	}
-	
-	//방에 있는 유저들의 채팅소켓의 스트림을 가져옴
-	public Hashtable<String, DataOutputStream> getUserListChat(){
+
+	// 방에 있는 유저들의 채팅소켓의 스트림을 가져옴
+	public Hashtable<String, DataOutputStream> getUserListChat() {
 		return user.getUserListChat();
+	}
+
+	// 내 방에 있는 해당유저 게임소켓의 스트림을 가져옴
+	public DataOutputStream getUserListGame(String name) {
+		return user.getUserListGame(name);
+	}
+
+	// 방에 있는 해당유저 채팅소켓의 스트림을 가져옴
+	public DataOutputStream getUserListChat(String name) {
+		return user.getUserListChat(name);
+	}
+	
+	//총 유저의 리스트 반환
+	public Enumeration<String> getUserNameList(){
+		return user.getUserListChat().keys();
 	}
 }
