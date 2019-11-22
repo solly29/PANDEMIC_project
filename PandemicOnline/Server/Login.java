ddb46960a508c5a2489279ac06818d4de8d562c9
dudrms9373 <55429152+dudrms9373@users.noreply.github.com>
2019-11-22 13:47:56 +0900

    DB 연결 ID, PWD 확인

--- PandemicOnline/Server/Login.java
+++ PandemicOnline/Server/Login.java
@@ -6,12 +6,15 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Set;
 
+import DB.DOA;
+
 public class Login{
 	private DataInputStream input = null;
 	private Socket gameSocket = null;
 	private Socket chatSocket = null;
 	private String ID;
 	private String PW;
+	DOA db = new DOA();
 	
 	public Login(Socket s1, Socket s2) {
 		// TODO Auto-generated constructor stub
@@ -19,10 +22,10 @@ public class Login{
 		chatSocket = s2;
 		
 		//임의의 유저
-		MainServer.IdPassword.put("admin","1234");
+		/*MainServer.IdPassword.put("admin","1234");
 		MainServer.IdPassword.put("user","1234");
 		MainServer.IdPassword.put("user2","1234");
-		MainServer.IdPassword.put("user3","1234");
+		MainServer.IdPassword.put("user3","1234");*/
 		try {
 			input = new DataInputStream(gameSocket.getInputStream());
 		} catch (IOException e) {
@@ -56,18 +59,13 @@ public class Login{
 			System.out.println("유저가 로그인 되어있음");
 			return false;
 		}
-		
-		if(MainServer.IdPassword.containsKey(ID)) {
-			System.out.println("ID 확인");
-			if(PW.equals(MainServer.IdPassword.get(ID))) {
+	
+			if(db.MatchPWD(ID,PW)) {
 				System.out.println("PW 일치");
 				return true;
 			}
-			return false;
-		}
-		else {
-			return false;
-		}
+			
+		return false;
 	}
 	//회원가입
 	public boolean join() {
@@ -86,7 +84,7 @@ public class Login{
 		}
 		
 		
-		if(MainServer.IdPassword.containsKey(ID)) {
+		if(!db.MatchPWD(ID,PW)) {
 			return false;
 		}
 		else {
