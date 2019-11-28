191ccd6b7ba2a85f13646ecf44f2f692c0cc614d
dudrms9373 <55429152+dudrms9373@users.noreply.github.com>
2019-11-28 23:37:21 +0900

    설명하는 주석 추가했습니다.

--- pandemicOnlineProject/src/DB/DOA.java
+++ pandemicOnlineProject/src/DB/DOA.java
@@ -16,7 +16,7 @@ public class DOA {
 	String url = "jdbc:mysql://13.125.178.148:3306/pandemic";
 	Socket socket;
 	DataOutputStream output;
-	public DOA(Socket socket) {
+	public DOA(Socket socket) { //소켓을 넣은 이유는 ID, PWD 찾을때 바로 클라로 넘겨주기 위함.
 		try {
 			this.socket = socket;
 			output = new DataOutputStream(socket.getOutputStream());
@@ -40,8 +40,8 @@ public class DOA {
 
 	}
 
-	public void Insert(String NAME,  String NUMBER, String ID, String PWD) {
-		
+	public void Insert(String NAME,  String NUMBER, String ID, String PWD) { 
+		// 회원가입이 성공적으로 이루어 지면 DB에 해당 정보 추가하는 메소드
 		StringBuilder sb = new StringBuilder();
 		
         String st = sb.append("INSERT INTO user_information (NAME,NUMBER, ID, PWD) VALUES('")
@@ -63,7 +63,7 @@ public class DOA {
 
 	}
 
-	public boolean MatchID(String ID) {
+	public boolean MatchID(String ID) {// 회원가입 할 때 아이디 중복체크 하는 메소드
 		try {
 
 			pstmt = conn.prepareStatement("SELECT PWD from user_information where ID = ?");
@@ -75,7 +75,7 @@ public class DOA {
 				String id = rs.getString("ID");
 				
 				if (id.equals(ID)) {
-					System.out.println("아이디 중복");
+				
 					return false;
 				}
 			}
@@ -87,23 +87,19 @@ public class DOA {
 
 	}
 
-	public boolean MatchPWD(String ID, String PWD) {
+	public boolean MatchPWD(String ID, String PWD) { //로그인할때 사용하는 메소드  ID, PWD 맞는지 확인
 		try {
 
-			pstmt = conn.prepareStatement("SELECT PWD, NAME, NUMBER from user_information where ID = ? ");
+			pstmt = conn.prepareStatement("SELECT PWDfrom user_information where ID = ? ");
 			pstmt.setString(1, ID);
 			rs = pstmt.executeQuery();
 
 			if(rs.next()) {
 
 				String pwd = rs.getString("PWD");
-				String name = rs.getString("NAME");
-				String number = rs.getString("NUMBER");
-				System.out.println(pwd + name + number);
-
+				
 				if (pwd.equals(PWD)) {
 
-					System.out.println("마자연!");
 					return true;
 				}
 			}
@@ -117,7 +113,7 @@ public class DOA {
 	}
 
 	
-	public boolean FindID(String NAME, String NUMBER) {
+	public boolean FindID(String NAME, String NUMBER) { // ID, PWD찾을때 사용하는 메소드
 		
 		try {
 
