9f380e47dff273430367df42394a64f5daa3f06b
dudrms9373 <55429152+dudrms9373@users.noreply.github.com>
2019-11-28 17:04:31 +0900

    업데이트~!

--- pandemicOnlineProject/src/Server/Login.java
+++ pandemicOnlineProject/src/Server/Login.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 import DB.DOA;
 
-public class Login{
+public class Login {
 	private DataInputStream input = null;
 	private Socket gameSocket = null;
 	private Socket chatSocket = null;
@@ -16,18 +16,21 @@ public class Login{
 	private String PW;
 	private String NAME;
 	private String NUMBER;
-	//DOA db = new DOA(); 지금은 디비 서버를 다시 만들어야되서 주석처리함
-	
+	DOA db;
+
 	public Login(Socket s1, Socket s2) {
 		// TODO Auto-generated constructor stub
 		gameSocket = s1;
 		chatSocket = s2;
-		
-		//임의의 유저
-		MainServer.IdPassword.put("admin","1234");
-		MainServer.IdPassword.put("user","1234");
-		MainServer.IdPassword.put("user2","1234");
-		MainServer.IdPassword.put("user3","1234");
+		db = new DOA(gameSocket);
+		// 임의의 유저
+		/*
+		 * MainServer.IdPassword.put("admin","1234");
+		 * MainServer.IdPassword.put("user","1234");
+		 * MainServer.IdPassword.put("user2","1234");
+		 * MainServer.IdPassword.put("user3","1234");
+		 */
+
 		try {
 			input = new DataInputStream(gameSocket.getInputStream());
 		} catch (IOException e) {
@@ -35,17 +38,16 @@ public class Login{
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public String getName() {
 		return ID;
 	}
-	
+
 	public boolean loginCheck() {
-		
+
 		ID = null;
 		PW = null;
-		
+
 		try {
 			ID = input.readUTF();
 			PW = input.readUTF();
@@ -54,48 +56,61 @@ public class Login{
 			e.printStackTrace();
 			return false;
 		}
-		
+
 		System.out.println("login");
-		
-		if(LobbyServer.userList.containsKey(ID)) {
+
+		if (LobbyServer.userList.containsKey(ID)) {
 			System.out.println("유저가 로그인 되어있음");
 			return false;
 		}
-		
-		if(MainServer.IdPassword.get(ID).equals(PW)) {
-			System.out.println("PW 일치");
-			return true;
-		}
-		
-		//디비서버를 다시 만들어야 되서 주석처리함
-		/*
+
 		if (db.MatchPWD(ID, PW)) {
 			System.out.println("PW 일치");
 			return true;
-		}*/
 
+			
+		}
 		return false;
 	}
-	//회원가입
+	//중복확인
+	public boolean duple() {
+		ID = null;
+		try {
+			ID = input.readUTF();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+
+		if (db.MatchID(ID)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	// 회원가입
 	public boolean join() {
 		ID = null;
 		PW = null;
 		NAME = null;
 		NUMBER = null;
-		
+
 		try {
 			ID = input.readUTF();
 			PW = input.readUTF();
 			NAME = input.readUTF();
 			NUMBER = input.readUTF();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 		}
-		
-		/* 디비가 원상복귀되면 주석 지우기
+
 		if (db.MatchID(ID)) {
 
 			db.Insert(NAME, NUMBER, ID, PW);
@@ -103,7 +118,32 @@ public class Login{
 			return true;
 		} else {
 			return false;
-		}*/
-		return false;//이거도 복구되면 지우기
+		}
+
+	}
+
+	public boolean find() {
+
+		NAME = null;
+		NUMBER = null;
+
+		try {
+
+			NAME = input.readUTF();
+			NUMBER = input.readUTF();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+
+		if (db.FindID(NAME, NUMBER)) {
+			
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
