6acd74f3e2fa40f2120d2be342532458b2baa285
good-game-yo <52366320+good-game-yo@users.noreply.github.com>
2019-12-06 18:08:43 +0900

    Update ESC.java

--- pandemicOnlineProject/src/Game/ESC.java
+++ pandemicOnlineProject/src/Game/ESC.java
@@ -12,7 +12,7 @@ public class ESC extends JOptionPane {
 				"정말로 종료 하시겠습니까? 지금 종료하시면 다시는 들어올 수 없습니다. 당신이 없으면 이 게임에서 승리 할 수 없어요!!", "나가기",
 				JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
 		if (i == 0) {
-			System.exit(Game.EXIT_ON_CLOSE); // 프레임을 종료한다
+			System.exit(Login.getTop().EXIT_ON_CLOSE); // 프레임을 종료한다
 		} else {
 			return;
 		}
